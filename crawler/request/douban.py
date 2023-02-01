17f572c0548d45f8581c0117c9722adad5ef3254
joshua-zhy <2147386300@qq.com>
2023-02-01 18:15:20 +0800

    crawel study for python

---
+++ crawler/request/douban.py
@@ -0,0 +1,19 @@
+import requests
+
+def crawl_douban():
+    url = "https://movie.douban.com/j/chart/top_list"
+    headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}
+    
+    params = {
+        "type": "11",
+        "interval_id": "100:90",
+        "action": "",
+        "start": 0,
+        "limit": 20
+    }
+    resp = requests.get(url=url,params=params,headers=headers).json()
+    # print(resp)
+    resp.close() #关闭连接
+if __name__ == '__main__':
+    crawl_douban()
+    
